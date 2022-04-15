fe0c35cf4296a671ac9e526a7c1900cbc135a961
palehazy <austinrpg@live.com>
2022-04-14 21:57:07 -0500

    adding to testing for class instance

---
+++ src/__tests__/class.test.ts
@@ -0,0 +1,13 @@
+import Jcrool from '../JcroolClass';
+
+const data = {
+    hello: "hi there sir",
+    tood: 42
+}
+const j = new Jcrool(data);
+
+describe("it finds pairs", () => {
+    test("twin", () => {
+        
+    })
+})
